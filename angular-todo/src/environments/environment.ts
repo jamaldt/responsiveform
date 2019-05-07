616d9070ffda0cc6e8daa541a4297faccc48ec0b
jamal <cdeltoro@consisint.com>
2019-05-07 09:00:40 -0400

    firebase config

--- angular-todo/src/environments/environment.ts
+++ angular-todo/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+   firebase : {
+    apiKey: "AIzaSyDjRmV2t2ENxlgRrSqsTIzSLrsfczoTrh8",
+    authDomain: "todo-app-a29c1.firebaseapp.com",
+    databaseURL: "https://todo-app-a29c1.firebaseio.com",
+    projectId: "todo-app-a29c1",
+    storageBucket: "todo-app-a29c1.appspot.com",
+    messagingSenderId: "644383920253",
+    appId: "1:644383920253:web:5f091ebfa2224e78"
+  }
 };
 
 /*
