860efa48de1dc399539a5e5d9cbc148fda20a17e
jamal_dt <carldeltoro@gmail.com>
2019-05-22 16:58:23 -0400

    netlify

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+   apiKey: "AIzaSyDjRmV2t2ENxlgRrSqsTIzSLrsfczoTrh8",
+   authDomain: "todo-app-a29c1.firebaseapp.com",
+   databaseURL: "https://todo-app-a29c1.firebaseio.com",
+   projectId: "todo-app-a29c1",
+   storageBucket: "todo-app-a29c1.appspot.com",
+   messagingSenderId: "644383920253",
+   appId: "1:644383920253:web:5f091ebfa2224e78"
+ }
 };
